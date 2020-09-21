b90b1ef4aa81f0e480883dcfaa1fdb5b9d335064
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-09-21 13:11:41 +0900

    캐스팅: 데이터 형 변환

---
+++ Letter.java
@@ -0,0 +1,15 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		
+		
+		String name = "leezsche";
+		System.out.println("Hello, "+name+" ... "+name+" ... "+name+" ...egoing ... bye");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+
+	}
+
+}
---
+++ Variable.java
@@ -0,0 +1,16 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // Number -> integer ... -2, -1, 
+		System.out.println(a);
+		
+		double b = 1.1; // real number -> double ... -2.0, -1.0, 0, 1, 1.0, 2.2, ...
+		System.out.println(b);
+		
+		String c = "Hello World";  // String
+		System.out.println(c);
+	}
+
+}
